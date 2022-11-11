cfc78a3364bfcee1ee3cb92d6fb31f66e18986db
Andrés Marcote Fernandez <amarfer@aulaestudio.es>
2022-11-11 19:33:47 +0100

    Update

--- JS/tiendaOnline/app.js
+++ JS/tiendaOnline/app.js
@@ -1,60 +1,60 @@
-const { calculateObjectSize } = require('bson');
-const express = require('express');
-const mongoose = require('mongoose');
-const app = express();
+// import express from 'express';
+// import { connect as _connect, Schema, model } from 'mongoose';
+// const app = express();
 
-const uri = 'mongodb+srv://Marcote:12345@cluster0.aniim6e.mongodb.net/?retryWrites=true&w=majority'
+// const uri = 'mongodb+srv://Marcote:12345@cluster0.aniim6e.mongodb.net/?retryWrites=true&w=majority'
 
-async function connect() {
-    try {
-        await mongoose.connect(uri);
-        console.log("Connected to MongoDB")
-    } catch (err) {
-        console.error(err)
-    }
-}
+// async function connect() {
+//     try {
+//         await _connect(uri);
+//         console.log("Connected to MongoDB")
+//     } catch (err) {
+//         console.error(err)
+//     }
+// }
 
-connect();
+// connect();
 
-async function create() {
-    const productSchema = new mongoose.Schema({
-        id: String,
-        name: String
-    }, { autoCreate: false, autoIndex: false });
-    const Product = mongoose.model('Product', productSchema);
+// export async function create() {
+//     console.log("Hago algo")
+//     const productSchema = new Schema({
+//         id: String,
+//         name: String
+//     }, { autoCreate: false, autoIndex: false });
+//     const Product = model('Product', productSchema);
 
-    const RedactedProduct = mongoose.model('RedactedProduct', productSchema);
+//     const RedactedProduct = model('RedactedProduct', productSchema);
+//     await Product.createCollection();
 
+//     await RedactedProduct.createCollection({
+//         viewOn: 'RedactedProduct', // Set `viewOn` to the collection name, **not** model name.
+//         pipeline: [
+//             {
+//                 $set: {
+//                     id: '$id',
+//                     name: '$name'
+//                 }
+//             }
+//         ]
+//     });
+//     await Product.create([
+//         {
+//             id: 21312313,
+//             name: "GTX 1080 TI"
+//         }
+//     ])
 
+//     await Product.find({ name: "GTX 1080 TI" });
+// }
 
-    await Product.createCollection();
+// app.listen(8000, () => {
+//     console.log("Server started on port 8000!")
+// })
 
-    await RedactedProduct.createCollection({
-        viewOn: 'RedactedProduct', // Set `viewOn` to the collection name, **not** model name.
-        pipeline: [
-            {
-                $set: {
-                    id: '$id',
-                    name: '$name'
-                }
-            }
-        ]
-    });
-    // await Product.create([
-    //     {
-    //         id: 21312313,
-    //         name: "GTX 1080 TI"
-    //     }
-    // ])
+import { readFile} from 'fs-web';
 
-    await Product.find({ name: "GTX 1080 TI" }).deleteMany();
-}
+readFile('/cesta.json', '/')
 
-create();
-
-
-
-app.listen(8000, () => {
-    console.log("Server started on port 8000!")
-})
-
+    .then((files) => {
+        console.log(files)
+    })
