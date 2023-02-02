996f0581b8031953b058b842cf87c46972de7fa7
Andrés Marcote Fernandez <amarfer@aulaestudio.es>
2023-02-02 18:54:58 +0100

    Update

---
+++ PRUEBA/main.js
@@ -0,0 +1,28 @@
+"use strict";
+
+function countBy(x, n) {
+    let z = [];
+    for (let i = x; z.length < n; i = i + x) {
+        z.push(i);
+    }
+    return z;
+}
+
+console.log(countBy(2, 5));
+
+function findEvenIndex(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        let suma = 0;
+        if (arr[i - 1] === undefined) {
+            arr[i - 1] = 0;
+        }
+        for (let j = i + 1; j < arr.length; j++) {
+            suma += arr[j];
+        }
+        if (suma == arr[i]) {
+            return i;
+        }
+    }
+}
+
+console.log(findEvenIndex([1, 2, 3, 4, 3, 2, 1]));
