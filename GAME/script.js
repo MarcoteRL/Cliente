6a019f663d844d723d78ae6b855309f7309e9cc0
Marcote <andres@Marcote.localdomain>
2023-02-11 15:38:23 +0100

    Update
    Committer: Marcote <andresmarcote13@gmail.com>

--- GAME/script.js
+++ GAME/script.js
@@ -10,47 +10,23 @@ const scaledCanvas = {
     height: canvas.height / 4
 }
 
+const floorCollisions2D = [];
+for (let i = 0; i < floorCollisions.length; i += 36) {
+    floorCollisions2D.push(floorCollisions.slice(i, i + 36));
+}
+const collisionBlocks = [];
+floorCollisions2D.forEach((row, y) => {
+    row.forEach((symbol, x) => {
+        if (symbol == 192) {
+            collisionBlocks.push(new CollisionBlock({
+                position: { x: x * 16, y: y * 16 }
+            }))
+        }
+    })
+})
+
 const gravity = 0.3;
 
-class Sprite {
-    constructor({ position, imageSrc }) {
-        this.position = position;
-        this.image = new Image();
-        this.image.src = imageSrc;
-    }
-
-    draw() {
-        if (!this.image) return;
-        c.drawImage(this.image, this.position.x, this.position.y)
-    }
-
-    update() {
-        this.draw();
-    }
-}
-
-class Player {
-    constructor(position) {
-        this.position = position;
-        this.velocity = { x: 0, y: 1 };
-        this.height = 100;
-    }
-    draw() {
-        c.fillStyle = "red";
-        c.fillRect(this.position.x, this.position.y, 100, this.height);
-    }
-
-    update() {
-        this.draw();
-        this.position.y += this.velocity.y;
-        this.position.x += this.velocity.x;
-        if (this.position.y + this.height + this.velocity.y < canvas.height) {
-            this.velocity.y += gravity;
-        } else {
-            this.velocity.y = 0;
-        }
-    }
-}
 
 const player = new Player({ x: 100, y: 100 });
 const player2 = new Player({ x: 400, y: 9 });
@@ -72,9 +48,12 @@ function animate() {
     c.fillStyle = "white";
     c.fillRect(0, 0, canvas.width, canvas.height);
     c.save()
-    c.scale(4, 4);
-    c.translate(0, -background.image.height + scaledCanvas.height)
+    // c.scale(4, 4);
+    // c.translate(0, -background.image.height + scaledCanvas.height)
     background.update();
+    collisionBlocks.forEach((collisionBlock) => {
+        collisionBlock.update()
+    })
     c.restore()
     player.update();
     player2.update();
